92774476e62de93b630b2c12716eba14f4055a50
kellybreedlove <kelly.breedlove@cslab05.wheaton.edu>
2015-02-02 21:33:36 -0600

    Linear Term

---
+++ TestVar.py
@@ -0,0 +1,15 @@
+import Var
+import unittest
+
+class TestVar(unittest.TestCase):
+  """Test Var.py's Id() method"""
+  def testid(self):
+   #self.assertAlmostEqual(8,Adder.addNumbers(5,3),delta=1e-12)
+   #self.assertEqual(8,Adder.addNumbers(5,3))
+  
+def testname(self):
+#test
+
+# Run the tests:
+if (__name__ == '__main__'):
+  unittest.main()
